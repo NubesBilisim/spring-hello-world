4f953c032b1041e296e999de21259bcfaf125ec9
Senay Girgin <senay.girgin@nubesbilisim.com>
2024-07-22 13:51:09 +0300

    Read from properties file.

--- src/main/java/com/nubes/springhelloworld/HelloWorldService.java
+++ src/main/java/com/nubes/springhelloworld/HelloWorldService.java
@@ -1,14 +1,17 @@
 package com.nubes.springhelloworld;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldService {
+    @Value("${hello: Default Hello}")
+    private String hello;
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return this.hello;
     }
 }
